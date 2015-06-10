47dab2994e5a11e94bd072b36e7211d4bb179589
Natalia Makarenko <makarenko.natalli@gmail.com>
2015-06-10 18:51:32 +0300

    registration

---
+++ library/checkAgent.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nmakarenko
+ * Date: 10.06.15
+ * Time: 18:08
+ */
+function checkAgent ($db, $login, $pass) {
+    $res = array();
+
+    $query2 = $db->prepare("SELECT password FROM agents WHERE login = :login");
+    $query2->bindParam(':login', $login, PDO::PARAM_STR);
+
+    $query2->execute();
+    while ($row = $query2->fetch(PDO::FETCH_ASSOC)) {
+        $res[] = $row;
+    }
+
+
+    if (!empty($res) && $res[0]['password'] == crypt($pass, 'CRYPT_SHA256')) {
+        return true;
+    }
+
+    return false;
+}
