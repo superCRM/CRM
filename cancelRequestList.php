2b3c477963cfb9b2e8157895e4d4951ae58a515d
Natalia Makarenko <makarenko.natalli@gmail.com>
2015-06-12 20:23:53 +0300

    + addRefund

--- cancelRequestList.php
+++ cancelRequestList.php
@@ -8,16 +8,30 @@
 include_once 'header.html';
 include_once 'library/db.php';
 include_once 'library/getRefundList.php';
+include_once ('library/createRefundItem.php');
 
-$cancelRequestList = getRefundList(getConnect(),0);
+$cancelRequestList = getRefundList(getConnect(), 0);
+
+
+var_dump($_POST);
+
+if(array_key_exists('but',$_POST)) {
+    if (isset($_POST['email']) && isset($_POST['product']) && isset($_POST['quantity']) &&
+        createRefund(getConnect(), $_POST['email'], $_POST['product'], $_POST['quantity'], 0)) {
+        header("Location: cancelRequestList.php");
+        echo "67";
+        //   exit();
+    }
+}
+
 ?>
-
     <div class="border-form">
         <table class="table table-hover">
             <thead>
                 <tr>
-                    <th>Order number</th>
                     <th>User email</th>
+                    <th>Product</th>
+                    <th>Quantity</th>
                     <th>Date</th>
                     <th></th>
                 </tr>
@@ -25,12 +39,21 @@ $cancelRequestList = getRefundList(getConnect(),0);
             <?php foreach($cancelRequestList as $request):?>
                 <tbody>
                     <tr class="success">
-                        <td><?=$request['order_num']?></td>
                         <td><?=$request['email_us']?></td>
+                        <td><?=$request['product']?></td>
+                        <td><?=$request['product_num']?></td>
                         <td><?=$request['date']?></td>
-                        <td><a href="cancelRequestItem.php?id=<?=$request['id']?>">Details</a> </td>
+                        <td><a href="cancelRequestItem.php?id=<?=$request['id']?>">Send</a> </td>
                     </tr>
                 </tbody>
             <?php endforeach ?>
+            <form class="form-horizontal" method = post>
+                <td><input type="text" class="form-control" name="email" id="inputPassword3" placeholder="Email"></td>
+                <td><input type="text" class="form-control" name="product" id="inputPassword3" placeholder="Product"></td>
+                <td><input type="text" class="form-control" name="quantity" id="inputPassword3" placeholder="Quantity"></td>
+                <td></td>
+                <td><button style="margin:5px;" type="submit" class="btn btn-primary" name="but" value="addButton">Add</button></td>
+            </form>
         </table>
+
     </div>
--- library/createRefundItem.php
+++ library/createRefundItem.php
@@ -6,67 +6,72 @@
  * Time: 19:54
  */
 
-function createRefund($db, $email, $order_num, $products){
+function createRefund($db, $email, $product, $product_num, $order_num){
+
 
     //checking data
+echo "$product_num";
+    $query = $db->prepare("SELECT * FROM users WHERE email = :email");
+    $query->bindParam(':email', $email, PDO::PARAM_STR);
 
-    for ($i = 0; $i < count($products); $i++) {
-        $res1 = array();
-        $res2 = array();
+    $query->execute();
 
-        $query = $db->prepare("SELECT sum FROM products WHERE order_id = :order_id AND name = :name");
-        $query->bindParam(':order_id', $order_num, PDO::PARAM_STR);
-        $query->bindParam(':name', $products[$i]['name'], PDO::PARAM_STR);
+    while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+        $res2[] = $row;
+    }
+    if(empty($res2)) {  return false;} // no users with this email
 
-        $query->execute();
-
-        while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-            $res1[] = $row;
-        }
-
-        $query = $db->prepare("SELECT refund_sum FROM products WHERE order_id = :order_id AND name = :name");
-        $query->bindParam(':order_id', $order_num, PDO::PARAM_STR);
-        $query->bindParam(':name', $products[$i]['name'], PDO::PARAM_STR);
+    $res2 = array();
+    if ($order_num == 0){
+        //find and set order_num
+        $query = $db->prepare("SELECT order_num FROM orders WHERE email_us = :email AND product = :product");
+        $query->bindParam(':product', $product, PDO::PARAM_STR);
+        $query->bindParam(':email', $email, PDO::PARAM_STR);
 
         $query->execute();
 
         while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
             $res2[] = $row;
         }
-
-        $diff = $res1['sum'] - $res2['refund_sum'];
-        if ($diff < $products[$i]['sum']) return 0;
+        $order_num = $res2[0]['order_num'];
     }
 
-    //addind
-    $res = array();
 
-        $query = $db->prepare("INSERT INTO refund (email_us, order_num, data, status)
-			 VALUES (:email, :order_num, now(),0)");
-        $query->bindParam(':order_num', $order_num, PDO::PARAM_STR);
+    $query = $db->prepare("SELECT prod_num, prod_num_refunded FROM orders WHERE email_us = :email AND order_num = :order_num AND product = :product");
+    $query->bindParam(':order_num', $order_num, PDO::PARAM_INT);
+    $query->bindParam(':product', $product, PDO::PARAM_STR);
+    $query->bindParam(':email', $email, PDO::PARAM_STR);
+
+    $query->execute();
+
+    while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+        $res1[] = $row;
+    }
+
+    $diff = $res1[0]['prod_num'] - $res1[0]['prod_num_refunded'];
+    echo "$product_num";
+    if (empty($res1) || $diff < $product_num) return false; //no order or refund sum is too big
+
+
+    //addind
+
+        $query = $db->prepare("INSERT INTO refund (email_us, product, date, product_num, status)
+			 VALUES (:email, :product, now(), :product_num, 0)");
+        $query->bindParam(':product_num', $product_num, PDO::PARAM_INT);
+        $query->bindParam(':product', $product, PDO::PARAM_STR);
         $query->bindParam(':email', $email, PDO::PARAM_STR);
 
         $query->execute();
-    $inserted_id = $db->lastInsertId();
 
-    for ($i = 0; $i < count($products); $i++) {
-        $query = $db->prepare("INSERT INTO refund_product (product_name, sum, id_refund)
-			 VALUES (:product_name, :sum, :id_refund)");
-        $query->bindParam(':product_name', $products[$i]['name'], PDO::PARAM_STR);
-        $query->bindParam(':sum', $products[$i]['sum']);
-        $query->bindParam(':id_refund', $inserted_id);
+        $query = $db->prepare("UPDATE orders SET prod_num_refunded = prod_num_refunded + :product_num
+                                WHERE email_us = :email AND product = :product AND order_num = :order_num");
+        $query->bindParam(':order_num', $order_num, PDO::PARAM_INT);
+        $query->bindParam(':product', $product, PDO::PARAM_STR);
+        $query->bindParam(':email', $email, PDO::PARAM_STR);
+        $query->bindParam(':product_num', $product_num, PDO::PARAM_INT);
 
         $query->execute();
 
-        $query = $db->prepare("UPDATE products SET refund_sum = refund_sum + :sum WHERE name = :name AND order_id = :order_num");
-        $query->bindParam(':sum', $products[$i]['sum']);
-        $query->bindParam(':name', $products[$i]['name'], PDO::PARAM_STR);
-        $query->bindParam(':order_num', $order_num, PDO::PARAM_STR);
-        $query->execute();
-    }
-
-
-
-    return 1;
+    return true;
 
 }
