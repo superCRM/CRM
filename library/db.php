408b77c190c261b3192aa63c487f7d7f7b98c83f
pasha <kovalp7@gmail.com>
2015-06-12 20:17:17 +0300

    Работает добавление юзеров

--- library/db.php
+++ library/db.php
@@ -1,7 +1,7 @@
 <?php
 
 function getConnect() {
-	$config = parse_ini_file('/var/www/dev.school-server/www/CRM/config/db.ini');
+	$config = parse_ini_file('/var/www/dev.school-server/www/dev/config/db.ini');
 	$db = new PDO ("mysql:host={$config['host']}; dbname={$config['db_name']}", $config['user'], $config['password']);
 
 	return $db;
