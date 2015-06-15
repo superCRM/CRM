76f23787a154d36c817f94a81bd7924dcb489e61
Natalia Makarenko <makarenko.natalli@gmail.com>
2015-06-15 16:31:36 +0300

    + getOrderList.php

---
+++ library/getOrderList.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nmakarenko
+ * Date: 15.06.15
+ * Time: 16:27
+ */
+
+function getOrderList ($ds, $email) {
+
+    $query = $db->prepare("SELECT order_num, product, prod_num, prod_num_refunded from orders WHERE email_us = :email");
+    $query->bindParam(':email', $email, PDO::PARAM_STR);
+
+    $query->execute();
+
+    while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+        $res[] = $row;
+    }
+
+    return $res;
+}
