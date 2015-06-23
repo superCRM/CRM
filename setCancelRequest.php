6ccfb6ff99603f18b08e06c6ed882316ec06273a
Natalia Makarenko <makarenko.natalli@gmail.com>
2015-06-23 18:33:48 +0300

    setCR

--- setCancelRequest.php
+++ setCancelRequest.php
@@ -14,15 +14,10 @@ include_once 'library/createRefundItem.php';
 
 if (!isset($_SESSION['refundList'])) {
     $refundList = array();
-    $_SESSION['refundList'] = $refundList;
+//    $_SESSION['refundList'] = $refundList;
 }
 
-$orderList = array();
-$ordr = array();
-$sum = 0;
-$numb = 0;
 
-$keys = array();
 
 session_start();
 
@@ -43,85 +38,89 @@ if (!isset($_POST['email']) &&  !isset($_POST['addButton'])) {
     <br>
 
     <?php
-    if (isset($_POST['email'])) $_SESSION['user_email'] = $_POST['email'];
+
     }
+    if (isset($_POST['email'])) $_SESSION['user_email'] = $_POST['email'];
 
     if (isset($_POST['addButton'])) {
 
         $refundList = $_SESSION['refundList'];
     }
-    else $refundList = array();
+  //  else $refundList = array();
 
     if (isset($_POST['email']) || isset($_POST['addButton'])) {
 
     echo "Adding cancel request to user {$_SESSION['user_email']}.";
- //   $orderList = getOrderList(getConnect(), $_POST['email']);
-?>
 
-    <form action = "" method = "post">
+    if (isset($_SESSION['refundList'])) {
+        //   $orderList = getOrderList(getConnect(), $_POST['email']);
+        ?>
+
+        <form action="" method="post">
 
 
         <table class="table table-hover">
-            <thead>
-            <tr>
+        <thead>
+        <tr>
 
-                <th>Key id</th>
-                <th>Percent</th>
-                <th>Deactivate?</th>
-                <th></th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php for ($i = 0; $i < count($refundList); $i++) { ?>
+            <th>Key id</th>
+            <th>Deactivate?</th>
+            <th></th>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php for ($i = 0; $i < count($refundList); $i++) { ?>
 
-                <tr class="success">
-                    <td><?= $refundList[$i]['key_id'] ?></td>
-                    <td><?= $refundList[$i]['percent'] ?>%</td>
-                    <td><input type="radio" name="deactiv" value="<?= $refundList[$i]['deactivate'] ?>"></td>
+            <tr class="success">
+                <td><?= $refundList[$i] ?></td>
+                <td><input type="checkbox" name="keyToCancel[]" value="<?= $refundList[$i] ?>"/></td>
 
-                    <td>
-                        <button style="margin:5px;" type="submit" class="btn btn-primary" name="deleteButton" value="deleteButton">
-                            Del
-                        </button>
-                    </td>
-
-                </tr>
-            <?php } ?>
-            <tr>
-                <td><input type="text" name="key_id" placeholder="Key_id"></td>
-                <td><input type="text" name="percent" placeholder="Percent">%</td>
-                <td></td>
                 <td>
-                    <button style="margin:5px;" type="submit" class="btn btn-primary" name="addButton" value="addButton">
-                        Add
+                    <button style="margin:5px;" type="submit" class="btn btn-primary" name="deleteButton"
+                            value="deleteButton">
+                        Del
                     </button>
                 </td>
-            </tr>
 
-            </tbody>
-        </table>
-        <button style="margin:5px;" type="submit" class="btn btn-primary" name="submitButton" value="submitButton">
-            Submit
-        </button>
-    </form>
+            </tr>
+        <?php }
+    }?>
+                <tr>
+                    <td><input type="text" name="key_id" placeholder="Key_id"></td>
+
+                    <td></td>
+
+                    <td>
+                        <button style="margin:5px;" type="submit" class="btn btn-primary" name="addButton"
+                                value="addButton">
+                            Add
+                        </button>
+                    </td>
+                </tr>
+
+                </tbody>
+            </table>
+            <td><input type="text" name="percent" placeholder="Percent">%</td>
+            <td><button style="margin:5px;" type="submit" class="btn btn-primary" name="submitButton" value="submitButton">
+                Submit
+            </button></td>
+        </form>
 
     <?php
 
 }
+if (isset($_POST['submitButton'])) unset($_SESSION['refundList']);
     var_dump($_POST);
 
-if (isset($_POST['key_id']) && isset($_POST['percent'])) {
+if (isset($_POST['key_id']) && $_POST['key_id'] != "") {
     // array_push($refundList, array('key_id' => $_POST['key_id'], 'percent' => $_POST['percent']));
     $number = count($refundList);
     echo "$number";
-    $refundList[$number]['key_id'] = $_POST['key_id'];
-    $refundList[$number]['percent'] = $_POST['percent'];
-    $refundList[$number]['deactivate'] = $number;
+    $refundList[$number] = $_POST['key_id'];
     var_dump($refundList);
     $_SESSION['refundList'] = $refundList;
     $_POST['key_id'] = null;
-    $_POST['percent'] = null;
 }
 
 ?>
