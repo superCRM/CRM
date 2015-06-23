2ba90a8f66f9e6b13f38707d233abe496f1a6038
Natalia Makarenko <makarenko.natalli@gmail.com>
2015-06-23 20:23:41 +0300

    setCR

--- setCancelRequest.php
+++ setCancelRequest.php
@@ -14,14 +14,14 @@ include_once 'library/createRefundItem.php';
 
 if (!isset($_SESSION['refundList'])) {
     $refundList = array();
-//    $_SESSION['refundList'] = $refundList;
+    $_SESSION['refundList'] = $refundList;
 }
 
 
 
 session_start();
 
-if (!isset($_POST['email']) &&  !isset($_POST['addButton'])) {
+if (!isset($_POST['email']) &&  !isset($_POST['addButton']) && !isset($_POST['deleteButton'])) {
 ?>
 
 <div class="col-md-3"></div>
@@ -30,7 +30,7 @@ if (!isset($_POST['email']) &&  !isset($_POST['addButton'])) {
         <form class="form-inline" method="post">
             <input type="email" name="email" placeholder="Email">
             <!--<button style="margin:5px;" type="submit" class="btn btn-primary" name="searchButton" value="searchButton">Search</button>-->
-            <button style="margin:5px;" type="submit" class="btn btn-primary" name="submitEmail" value="submitButton">
+            <button style="margin:5px;" type="submit" class="btn btn-primary" name="submitEmail" value="submitEmail">
                 Submit
             </button>
         </form>
@@ -42,86 +42,95 @@ if (!isset($_POST['email']) &&  !isset($_POST['addButton'])) {
     }
     if (isset($_POST['email'])) $_SESSION['user_email'] = $_POST['email'];
 
-    if (isset($_POST['addButton'])) {
+    if (isset($_SESSION['refundList'])) {
 
         $refundList = $_SESSION['refundList'];
     }
   //  else $refundList = array();
 
-    if (isset($_POST['email']) || isset($_POST['addButton'])) {
+    if (isset($_POST['email']) || isset($_POST['addButton']) || isset($_POST['deleteButton'])) {
 
-    echo "Adding cancel request to user {$_SESSION['user_email']}.";
+    echo "Adding cancel request to user {$_SESSION['user_email']}.<br>";
 
-    if (isset($_SESSION['refundList'])) {
+
         //   $orderList = getOrderList(getConnect(), $_POST['email']);
         ?>
 
-        <form action="" method="post">
+        <form class="form-inline border-form col-md-8" action="setCancelRequest.php" method="post">
 
 
-        <table class="table table-hover">
-        <thead>
-        <tr>
-
-            <th>Key id</th>
-            <th>Deactivate?</th>
-            <th></th>
-            <th></th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php for ($i = 0; $i < count($refundList); $i++) { ?>
-
-            <tr class="success">
-                <td><?= $refundList[$i] ?></td>
-                <td><input type="checkbox" name="keyToCancel[]" value="<?= $refundList[$i] ?>"/></td>
-
-                <td>
-                    <button style="margin:5px;" type="submit" class="btn btn-primary" name="deleteButton"
-                            value="deleteButton">
-                        Del
-                    </button>
-                </td>
-
-            </tr>
-        <?php }
-    }?>
+            <table class="table table-hover">
+                <?php if (isset($_SESSION['refundList'])) { ?>
+                <thead>
                 <tr>
-                    <td><input type="text" name="key_id" placeholder="Key_id"></td>
 
-                    <td></td>
+                    <th>Key id</th>
+                    <th>Deactivate?</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                    foreach($refundList as $refund) : ?>
 
-                    <td>
+                        <tr class="success">
+                            <td><?= $refund ?></td>
+                            <td><input type="checkbox" name="keyToCancel[]" value="<?= $refund ?>"/></td>
+
+                            <td>
+                                <button style="margin:5px;" type="submit" class="btn btn-primary" name="deleteButton"
+                                        value="<?= $refund  ?>">
+                                    Delete
+                                </button>
+                            </td>
+
+                        </tr>
+                    <?php endforeach;
+                } ?>
+                </tbody>
+            </table>
+            </form>
+        <br>
+            <input type="text" name="key_id" placeholder="Key_id">
                         <button style="margin:5px;" type="submit" class="btn btn-primary" name="addButton"
                                 value="addButton">
                             Add
                         </button>
-                    </td>
-                </tr>
+            <br>
 
-                </tbody>
-            </table>
-            <td><input type="text" name="percent" placeholder="Percent">%</td>
-            <td><button style="margin:5px;" type="submit" class="btn btn-primary" name="submitButton" value="submitButton">
+            <input type="text" name="percent" placeholder="Percent">%
+            <button style="margin:5px;" type="submit" class="btn btn-primary" name="submitButton" value="submitButton">
                 Submit
-            </button></td>
-        </form>
+            </button>
+
 
     <?php
 
 }
-if (isset($_POST['submitButton'])) unset($_SESSION['refundList']);
-    var_dump($_POST);
-
-if (isset($_POST['key_id']) && $_POST['key_id'] != "") {
-    // array_push($refundList, array('key_id' => $_POST['key_id'], 'percent' => $_POST['percent']));
-    $number = count($refundList);
-    echo "$number";
-    $refundList[$number] = $_POST['key_id'];
-    var_dump($refundList);
-    $_SESSION['refundList'] = $refundList;
-    $_POST['key_id'] = null;
+if (isset($_POST['submitButton'])) {
+    $_POST['refundList'] = $_SESSION['refundList'];
+    unset($_SESSION['refundList']);
 }
+   var_dump($_POST);
+    var_dump($_SESSION);
+//    var_dump(in_array( $_POST['key_id'], $refundList));
+if (isset($_POST['deleteButton'])) {
+    $refundList = array_diff($refundList, array($_POST['deleteButton']));
+    $_SESSION['refundList'] = $refundList;
+}
+else
+    if (isset($_POST['key_id']) && $_POST['key_id'] != "" && !in_array($_POST['key_id'], $refundList)) {
+         array_push($refundList, $_POST['key_id']);
+//        $number = count($refundList);
+//        echo "$number";
+//        $refundList[$number] = $_POST['key_id'];
+        var_dump($refundList);
+        $_SESSION['refundList'] = $refundList;
+        unset($_POST['key_id']);
+
+     //   header("Location: setCancelRequest.php");
+    }
 
 ?>
 
