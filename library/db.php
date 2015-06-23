857ddfeccb7ba90acb28a79ec376a6c09f8bba7d
pasha <kovalp7@gmail.com>
2015-06-23 19:53:23 +0300

    0

--- library/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function getConnect() {
-	$config = parse_ini_file('/var/www/dev.school-server/www/dev/config/db.ini');
-	$db = new PDO ("mysql:host={$config['host']}; dbname={$config['db_name']}", $config['user'], $config['password']);
-
-	return $db;
-}
-
