2c8d4333066f7643837617cc4bc9ba2313b83f81
Natalia Makarenko <makarenko.natalli@gmail.com>
2015-06-26 19:54:30 +0300

    declaration of classes

---
+++ CRMClassLibrary/key.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nmakarenko
+ * Date: 26.06.15
+ * Time: 18:48
+ */
+namespace CRM;
+
+class Key {
+    public $keyId;
+    public $orderId;
+    public $status;
+    public $percent;
+
+    public function createKey(){
+
+    }
+
+    public function getKey($id){
+
+    }
+
+    public function getKeyStatus($id){
+
+    }
+
+    public function getKeysByRefund($id){
+
+    }
+
+    public function getKeysByOrder($id){
+
+    }
+
+    public function changeKeyStatus($id, $status){
+
+    }
+
+    public function decrementKeyPercent($id, $percent){
+
+    }
+}
+
